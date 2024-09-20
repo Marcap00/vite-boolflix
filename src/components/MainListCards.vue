179a2fbd5b0502f40fba9f22b329b1cbd9b8a674
Marcap00 <cappellatomarco@yahoo.com>
2024-09-20 17:01:56 +0200

    made a call to the movie db api and memorized in store

--- src/components/MainListCards.vue
+++ src/components/MainListCards.vue
@@ -1,13 +1,40 @@
 <script>
+import { store } from '../store.js';
+import axios from 'axios';
 import CardsItem from './ListCardsItem.vue';
 export default {
     data() {
         return {
-
+            urlApi: 'https://api.themoviedb.org/3/search/movie'
         }
     },
     components: {
+        store,
         CardsItem
+    },
+    methods: {
+        getMovies() {
+            axios.get(this.urlApi, {
+                params: {
+                    api_key: "df92fe028bcac745150cea6e094cf605",
+                    query: 'ritorno al futuro'
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.results);
+                    store.movies = response.data.results;
+                    console.log('Dato memorizzato nello store:', store.movies);
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+                .finally(() => {
+                    console.log('========Chiamata Api effettuata======');
+                });
+        }
+    },
+    created() {
+        this.getMovies();
     }
 };
 </script>
@@ -16,7 +43,6 @@ export default {
     <div class="row">
         <CardsItem />
     </div>
-
 </template>
 
 <style lang="scss">
