c95f5b6edcb7f191daf4ec7412a7e126b988be79
Marcap00 <cappellatomarco@yahoo.com>
2024-09-20 15:47:09 +0200

    added styles to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,8 +9,26 @@ export default {
 </script>
 
 <template>
-    <h2>Header</h2>
-
+    <header>
+        <h1>BOOLFLIX</h1>
+        <input type="text" placeholder="Inserisci il titolo di un film">
+    </header>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+header {
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 1rem;
+
+    background-color: #000;
+    color: red;
+
+    h1 {
+        font-weight: 500;
+    }
+}
+</style>
